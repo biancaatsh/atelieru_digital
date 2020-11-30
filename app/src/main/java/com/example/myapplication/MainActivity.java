5c81bbae0d58c3aaa52db5a5eb2fb47728343df2
biancaatsh <57296028+biancaatsh@users.noreply.github.com>
2020-11-30 14:10:17 +0200

    My first code challenge

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
